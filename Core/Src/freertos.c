8befdaa3608f62943d7ef31281a581ccd3145c49
halo-zh <zh.hao@outlook.com>
2021-08-31 17:44:56 +0800

    format c file

--- Core/Src/freertos.c
+++ Core/Src/freertos.c
@@ -38,7 +38,7 @@
 
 /* Private define ------------------------------------------------------------*/
 /* USER CODE BEGIN PD */
-#define PubTopic         "/sys/a1fkV4RfRSP/AQ_SCOOTER_TEST/thing/event/property/post"
+#define PubTopic "/sys/a1fkV4RfRSP/AQ_SCOOTER_TEST/thing/event/property/post"
 /* USER CODE END PD */
 
 /* Private macro -------------------------------------------------------------*/
@@ -46,8 +46,7 @@
 extern uint8_t gprsStatus;
 extern uint8_t gnssDataAval;
 
-
-uint32_t eventData=0;
+uint32_t eventData = 0;
 
 /* USER CODE END PM */
 
@@ -61,14 +60,14 @@ osTimerId readBatInfoTimerHandle;
 osMessageQId cloudQueueHandle;
 enum CLOUD_EVENT_ID
 {
-  CLOUD_NULL=0,
+  CLOUD_NULL = 0,
   CLOUD_UPLOAD_INFO,
   CLOUD_UPLOAD_GPS,
   CLOUD_UPLOAD_BAT_ERR,
   CLOUD_UPLOAD_INVERTER_ERR,
   GPS_INIT,
   READ_GPS_DATA,
- 
+
 };
 /* USER CODE END Variables */
 osThreadId defaultTaskHandle;
@@ -79,34 +78,34 @@ osStaticSemaphoreDef_t sempRxmsgRdyControlBlock;
 
 /* Private function prototypes -----------------------------------------------*/
 /* USER CODE BEGIN FunctionPrototypes */
-void ioControlFunc(void const * argument);   
-void gpsTimerFunc(void const * argument);
-void cloudTimerFunc(void const * argument);
+void ioControlFunc(void const *argument);
+void gpsTimerFunc(void const *argument);
+void cloudTimerFunc(void const *argument);
 void cloud_queue_exec(osEvent *osEvent);
-void errorTimerFunc(void const* argument);
-void readBatInfoFunc(void const* argument);
+void errorTimerFunc(void const *argument);
+void readBatInfoFunc(void const *argument);
 /* USER CODE END FunctionPrototypes */
 
-void StartDefaultTask(void const * argument);
-void mqtt_process_thread(void const * argument);
-void mqtt_recv_thread(void const * argument);
+void StartDefaultTask(void const *argument);
+void mqtt_process_thread(void const *argument);
+void mqtt_recv_thread(void const *argument);
 
 void MX_FREERTOS_Init(void); /* (MISRA C 2004 rule 8.1) */
 
 /* GetIdleTaskMemory prototype (linked to static allocation support) */
-void vApplicationGetIdleTaskMemory( StaticTask_t **ppxIdleTaskTCBBuffer, StackType_t **ppxIdleTaskStackBuffer, uint32_t *pulIdleTaskStackSize );
+void vApplicationGetIdleTaskMemory(StaticTask_t **ppxIdleTaskTCBBuffer, StackType_t **ppxIdleTaskStackBuffer, uint32_t *pulIdleTaskStackSize);
 
 /* GetTimerTaskMemory prototype (linked to static allocation support) */
-void vApplicationGetTimerTaskMemory( StaticTask_t **ppxTimerTaskTCBBuffer, StackType_t **ppxTimerTaskStackBuffer, uint32_t *pulTimerTaskStackSize );
+void vApplicationGetTimerTaskMemory(StaticTask_t **ppxTimerTaskTCBBuffer, StackType_t **ppxTimerTaskStackBuffer, uint32_t *pulTimerTaskStackSize);
 
 /* Hook prototypes */
 void vApplicationIdleHook(void);
 void vApplicationTickHook(void);
 
 /* USER CODE BEGIN 2 */
-__weak void vApplicationIdleHook( void )
+__weak void vApplicationIdleHook(void)
 {
-   /* vApplicationIdleHook() will only be called if configUSE_IDLE_HOOK is set
+  /* vApplicationIdleHook() will only be called if configUSE_IDLE_HOOK is set
    to 1 in FreeRTOSConfig.h. It will be called on each iteration of the idle
    task. It is essential that code added to this hook function never attempts
    to block in any way (for example, call xQueueReceive() with a block time
@@ -119,9 +118,9 @@ __weak void vApplicationIdleHook( void )
 /* USER CODE END 2 */
 
 /* USER CODE BEGIN 3 */
-__weak void vApplicationTickHook( void )
+__weak void vApplicationTickHook(void)
 {
-   /* This function will be called by each tick interrupt if
+  /* This function will be called by each tick interrupt if
    configUSE_TICK_HOOK is set to 1 in FreeRTOSConfig.h. User code can be
    added here, but the tick hook is called from an interrupt context, so
    code must not attempt to block, and only the interrupt safe FreeRTOS API
@@ -133,7 +132,7 @@ __weak void vApplicationTickHook( void )
 static StaticTask_t xIdleTaskTCBBuffer;
 static StackType_t xIdleStack[configMINIMAL_STACK_SIZE];
 
-void vApplicationGetIdleTaskMemory( StaticTask_t **ppxIdleTaskTCBBuffer, StackType_t **ppxIdleTaskStackBuffer, uint32_t *pulIdleTaskStackSize )
+void vApplicationGetIdleTaskMemory(StaticTask_t **ppxIdleTaskTCBBuffer, StackType_t **ppxIdleTaskStackBuffer, uint32_t *pulIdleTaskStackSize)
 {
   *ppxIdleTaskTCBBuffer = &xIdleTaskTCBBuffer;
   *ppxIdleTaskStackBuffer = &xIdleStack[0];
@@ -146,7 +145,7 @@ void vApplicationGetIdleTaskMemory( StaticTask_t **ppxIdleTaskTCBBuffer, StackTy
 static StaticTask_t xTimerTaskTCBBuffer;
 static StackType_t xTimerStack[configTIMER_TASK_STACK_DEPTH];
 
-void vApplicationGetTimerTaskMemory( StaticTask_t **ppxTimerTaskTCBBuffer, StackType_t **ppxTimerTaskStackBuffer, uint32_t *pulTimerTaskStackSize )
+void vApplicationGetTimerTaskMemory(StaticTask_t **ppxTimerTaskTCBBuffer, StackType_t **ppxTimerTaskStackBuffer, uint32_t *pulTimerTaskStackSize)
 {
   *ppxTimerTaskTCBBuffer = &xTimerTaskTCBBuffer;
   *ppxTimerTaskStackBuffer = &xTimerStack[0];
@@ -160,9 +159,10 @@ void vApplicationGetTimerTaskMemory( StaticTask_t **ppxTimerTaskTCBBuffer, Stack
   * @param  None
   * @retval None
   */
-void MX_FREERTOS_Init(void) {
+void MX_FREERTOS_Init(void)
+{
   /* USER CODE BEGIN Init */
-       
+
   /* USER CODE END Init */
 
   /* USER CODE BEGIN RTOS_MUTEX */
@@ -180,26 +180,24 @@ void MX_FREERTOS_Init(void) {
 
   /* USER CODE BEGIN RTOS_TIMERS */
   /* start timers, add new ones, ... */
-  osTimerDef(ioControlTimer,ioControlFunc);
-  ioControlTimerHandle = osTimerCreate(osTimer(ioControlTimer),osTimerPeriodic,NULL);
-  osTimerStart(ioControlTimerHandle,100);
-  
-  osTimerDef(gpsTimer,gpsTimerFunc);
-  gpsTimerHandle = osTimerCreate(osTimer(gpsTimer),osTimerPeriodic,NULL);
-  
-  osTimerDef(readBatTimer,readBatInfoFunc);
-  readBatInfoTimerHandle = osTimerCreate(osTimer(readBatTimer),osTimerPeriodic,NULL);
-  
-  
-  osTimerDef(cloudTimer,cloudTimerFunc);
-  cloudTimerHandle = osTimerCreate(osTimer(cloudTimer),osTimerPeriodic,NULL);
-  
-  
-  osTimerDef(errorTimer,errorTimerFunc);
-  errorTimerHandle = osTimerCreate(osTimer(errorTimer),osTimerPeriodic,NULL);
+  osTimerDef(ioControlTimer, ioControlFunc);
+  ioControlTimerHandle = osTimerCreate(osTimer(ioControlTimer), osTimerPeriodic, NULL);
+  osTimerStart(ioControlTimerHandle, 100);
 
-  osMessageQDef(cloudQueue,50,uint32_t);
-  cloudQueueHandle = osMessageCreate(osMessageQ(cloudQueue),NULL);
+  osTimerDef(gpsTimer, gpsTimerFunc);
+  gpsTimerHandle = osTimerCreate(osTimer(gpsTimer), osTimerPeriodic, NULL);
+
+  osTimerDef(readBatTimer, readBatInfoFunc);
+  readBatInfoTimerHandle = osTimerCreate(osTimer(readBatTimer), osTimerPeriodic, NULL);
+
+  osTimerDef(cloudTimer, cloudTimerFunc);
+  cloudTimerHandle = osTimerCreate(osTimer(cloudTimer), osTimerPeriodic, NULL);
+
+  osTimerDef(errorTimer, errorTimerFunc);
+  errorTimerHandle = osTimerCreate(osTimer(errorTimer), osTimerPeriodic, NULL);
+
+  osMessageQDef(cloudQueue, 50, uint32_t);
+  cloudQueueHandle = osMessageCreate(osMessageQ(cloudQueue), NULL);
   /* USER CODE END RTOS_TIMERS */
 
   /* USER CODE BEGIN RTOS_QUEUES */
@@ -222,7 +220,6 @@ void MX_FREERTOS_Init(void) {
   /* USER CODE BEGIN RTOS_THREADS */
   /* add threads, ... */
   /* USER CODE END RTOS_THREADS */
-
 }
 
 /* USER CODE BEGIN Header_StartDefaultTask */
@@ -232,36 +229,33 @@ void MX_FREERTOS_Init(void) {
   * @retval None
   */
 /* USER CODE END Header_StartDefaultTask */
-void StartDefaultTask(void const * argument)
+void StartDefaultTask(void const *argument)
 {
   /* USER CODE BEGIN StartDefaultTask */
- 
-  
-  osTimerStart(readBatInfoTimerHandle,500);
-  
-  
+
+  osTimerStart(readBatInfoTimerHandle, 500);
+
   MC20_Init();
   GPSInit();
   MQTT_Start();
-  mqttStatus =1;
-  
-  osTimerStart(errorTimerHandle,10000);
-  osTimerStart(cloudTimerHandle,5000);
-  osTimerStart(gpsTimerHandle,7000);
-  
-  
-   HAL_GPIO_WritePin(DEBUG_EN_GPIO_Port,DEBUG_EN_Pin,ENABLE);
+  mqttStatus = 1;
+
+  osTimerStart(errorTimerHandle, 10000);
+  osTimerStart(cloudTimerHandle, 5000);
+  osTimerStart(gpsTimerHandle, 7000);
+
+  HAL_GPIO_WritePin(DEBUG_EN_GPIO_Port, DEBUG_EN_Pin, ENABLE);
   /* Infinite loop */
-  for(;;)
+  for (;;)
   {
-    
-     osEvent event;
-     event = osMessageGet(cloudQueueHandle,0);
-     if(osEventMessage == event.status)
-     {
-       cloud_queue_exec(&event);
-     }
-     osDelay(10);
+
+    osEvent event;
+    event = osMessageGet(cloudQueueHandle, 0);
+    if (osEventMessage == event.status)
+    {
+      cloud_queue_exec(&event);
+    }
+    osDelay(10);
   }
   /* USER CODE END StartDefaultTask */
 }
@@ -273,16 +267,14 @@ void StartDefaultTask(void const * argument)
 * @retval None
 */
 /* USER CODE END Header_mqtt_process_thread */
-void mqtt_process_thread(void const * argument)
+void mqtt_process_thread(void const *argument)
 {
   /* USER CODE BEGIN mqtt_process_thread */
   /* Infinite loop */
-  for(;;)
+  for (;;)
   {
 
-   osDelay(100);
-     
-
+    osDelay(100);
   }
   /* USER CODE END mqtt_process_thread */
 }
@@ -294,11 +286,11 @@ void mqtt_process_thread(void const * argument)
 * @retval None
 */
 /* USER CODE END Header_mqtt_recv_thread */
-void mqtt_recv_thread(void const * argument)
+void mqtt_recv_thread(void const *argument)
 {
   /* USER CODE BEGIN mqtt_recv_thread */
   /* Infinite loop */
-  for(;;)
+  for (;;)
   {
     osDelay(10000);
   }
@@ -309,144 +301,137 @@ void mqtt_recv_thread(void const * argument)
 /* USER CODE BEGIN Application */
 void stopAllTimer()
 {
-  
-   osTimerStop(readBatInfoTimerHandle); 
-   osTimerStop(errorTimerHandle);
-   osTimerStop(cloudTimerHandle);
-   osTimerStop(gpsTimerHandle);
+
+  osTimerStop(readBatInfoTimerHandle);
+  osTimerStop(errorTimerHandle);
+  osTimerStop(cloudTimerHandle);
+  osTimerStop(gpsTimerHandle);
 }
 
+uint8_t volt12VStatus = 0;
+void ioControlFunc(void const *argument)
+{
 
-uint8_t volt12VStatus=0;
- void ioControlFunc(void const * argument)
- {
-   
-     
-     if(gprsStatus ==1)
-     {
-        osTimerStart(ioControlTimerHandle,600);
-        HAL_GPIO_TogglePin(LED_Y_GPIO_Port,LED_Y_Pin);
-     }
-     else      
-     {
-       HAL_GPIO_WritePin(LED_Y_GPIO_Port, LED_Y_Pin, GPIO_PIN_SET);
-     }
-     if(gnssDataAval == 1)
-     {      
-       osTimerStart(ioControlTimerHandle,600);
-       HAL_GPIO_TogglePin(LED_R_GPIO_Port,LED_R_Pin);
-     }
-     else      
-     {
-       HAL_GPIO_WritePin(LED_R_GPIO_Port, LED_R_Pin, GPIO_PIN_SET);
-     }
-    
- }
- 
-  void gpsTimerFunc(void const * argument)
+  if (gprsStatus == 1)
   {
+    osTimerStart(ioControlTimerHandle, 600);
+    HAL_GPIO_TogglePin(LED_Y_GPIO_Port, LED_Y_Pin);
+  }
+  else
+  {
+    HAL_GPIO_WritePin(LED_Y_GPIO_Port, LED_Y_Pin, GPIO_PIN_SET);
+  }
+  if (gnssDataAval == 1)
+  {
+    osTimerStart(ioControlTimerHandle, 600);
+    HAL_GPIO_TogglePin(LED_R_GPIO_Port, LED_R_Pin);
+  }
+  else
+  {
+    HAL_GPIO_WritePin(LED_R_GPIO_Port, LED_R_Pin, GPIO_PIN_SET);
+  }
+}
 
-    if(gnssDataAval == 0)
-    {       
-       eventData = READ_GPS_DATA;
-       osMessagePut(cloudQueueHandle,eventData,0); 
-       return;
-    }
+void gpsTimerFunc(void const *argument)
+{
 
-  
-     eventData = READ_GPS_DATA;
-     osMessagePut(cloudQueueHandle,eventData,0); 
-     osDelay(10); 
-    
-    
- 
-    if(gnssDataAval == 1)  // Data valid then upload.otherwise no upload
+  if (gnssDataAval == 0)
+  {
+    eventData = READ_GPS_DATA;
+    osMessagePut(cloudQueueHandle, eventData, 0);
+    return;
+  }
+
+  eventData = READ_GPS_DATA;
+  osMessagePut(cloudQueueHandle, eventData, 0);
+  osDelay(10);
+
+  if (gnssDataAval == 1) // Data valid then upload.otherwise no upload
+  {
+    eventData = CLOUD_UPLOAD_GPS;
+    osMessagePut(cloudQueueHandle, eventData, 0);
+  }
+}
+
+CAN_TxHeaderTypeDef readVolt;
+
+uint8_t reqData[8] = {
+    0x46,
+    0x16,
+    0x1,
+    0,
+};
+uint32_t txMailBox = 0;
+
+HAL_StatusTypeDef sendCANMessage(uint8_t index, uint8_t len)
+{
+  reqData[3] = index;
+  reqData[4] = len;
+  reqData[5] = 0;
+
+  for (uint8_t i = 0; i < 5; i++)
+  {
+    reqData[5] += reqData[i];
+  }
+  HAL_CAN_AddTxMessage(&hcan2, &readVolt, reqData, &txMailBox);
+  HAL_CAN_AddTxMessage(&hcan1, &readVolt, reqData, &txMailBox);
+  return 0;
+}
+
+uint8_t index = 0;
+uint16_t batInsideVolt = 0;
+uint8_t sleepCondition = 0;
+uint8_t test = 0;
+uint8_t externalPwrStatus = 0;
+uint16_t delayCnt = 0;
+#define DELAY_CNT 2 * 300
+void readBatInfoFunc(void const *arg)
+{
+  /* 500ms periodic */
+  uint16_t tempAdc = 0;
+  uint16_t i = 0;
+
+  HAL_ADC_Start(&hadc1);
+  if (HAL_OK == HAL_ADC_PollForConversion(&hadc1, 2))
+  {
+    tempAdc = HAL_ADC_GetValue(&hadc1) * 330 * 2 / 4096;
+    batInsideVolt = tempAdc;
+
+    if (batInsideVolt < 330)
     {
-      eventData = CLOUD_UPLOAD_GPS;
-      osMessagePut(cloudQueueHandle,eventData,0); 
+      sleepCondition = 1;
+    }
+    else
+    {
+      sleepCondition = 0;
     }
   }
- 
- CAN_TxHeaderTypeDef readVolt;
- 
- uint8_t reqData[8]={0x46,0x16,0x1,0,};
- uint32_t txMailBox=0;
- 
- HAL_StatusTypeDef sendCANMessage(uint8_t index,uint8_t len)
- {
-    reqData[3] = index;
-    reqData[4] = len;
-    reqData[5]=0;
-    
-    for(uint8_t i=0;i<5;i++)
-    {
-      reqData[5] += reqData[i];
-    }
-    HAL_CAN_AddTxMessage(&hcan2,&readVolt,reqData,&txMailBox);
-    HAL_CAN_AddTxMessage(&hcan1,&readVolt,reqData,&txMailBox);
-    return 0;
-   
- }
- 
-uint8_t index=0;
-uint16_t batInsideVolt=0;
-uint8_t sleepCondition=0;
-uint8_t test=0;
-uint8_t externalPwrStatus=0;
-uint16_t delayCnt=0;
-#define DELAY_CNT  2*300
- void readBatInfoFunc(void const *arg)
- {
-   /* 500ms periodic */
-   uint16_t tempAdc=0;
-   uint16_t i=0;
-  
-     HAL_ADC_Start(&hadc1);
-     if(HAL_OK ==  HAL_ADC_PollForConversion(&hadc1,2))
-     {    
-        tempAdc = HAL_ADC_GetValue(&hadc1)*330*2/4096;
-        batInsideVolt = tempAdc;  
-        
-        if(batInsideVolt<330)
-        {
-          sleepCondition = 1;
-        }
-        else
-        {
-          sleepCondition = 0;
-        }
-        
-     }
-        
-                    
-      externalPwrStatus = HAL_GPIO_ReadPin(Power_Monitor_GPIO_Port,Power_Monitor_Pin); 
-      if(externalPwrStatus ==1)
-      {
-          delayCnt =0;
-      }
-      else
-      {
-         delayCnt++;
-      }
-      
-      if(delayCnt>DELAY_CNT)
-      {
-        sleepCondition = 1;
-      }
-      else
-      {
-        sleepCondition = 0;
-      }
-      
-      if(test ==1)
-      {
-         sleepCondition = test;
-      }
-   
-   
-  
-   
-   /*
+
+  externalPwrStatus = HAL_GPIO_ReadPin(Power_Monitor_GPIO_Port, Power_Monitor_Pin);
+  if (externalPwrStatus == 1)
+  {
+    delayCnt = 0;
+  }
+  else
+  {
+    delayCnt++;
+  }
+
+  if (delayCnt > DELAY_CNT)
+  {
+    sleepCondition = 1;
+  }
+  else
+  {
+    sleepCondition = 0;
+  }
+
+  if (test == 1)
+  {
+    sleepCondition = test;
+  }
+
+  /*
    readVolt.DLC=6;
    readVolt.IDE = CAN_ID_STD;
    readVolt.StdId = 0x528;
@@ -469,41 +454,38 @@ uint16_t delayCnt=0;
    }
    index++;
 */
- }
- 
- void cloudTimerFunc(void const * argument)
- {
-     if(gprsStatus == 1)
-     {
-        eventData = CLOUD_UPLOAD_INFO;
-        osMessagePut(cloudQueueHandle,eventData,0); 
-     }
-  }
- 
- void errorTimerFunc(void const * argument)
- {
-   
-   if(checkBatErr() == 1)
-    {
-      eventData = CLOUD_UPLOAD_BAT_ERR;
-      osMessagePut(cloudQueueHandle,eventData,0); 
-    }
-   
-   if(checkInverterErr() == 1)
-   {
-     eventData = CLOUD_UPLOAD_INVERTER_ERR;
-     osMessagePut(cloudQueueHandle,eventData,0); 
-   }
+}
 
-   
-   
- }
- 
- uint32_t event=0;
- void cloud_queue_exec(osEvent *osEvent)
- {
-     
-     event = (uint32_t)(osEvent->value.v);
+void cloudTimerFunc(void const *argument)
+{
+  if (gprsStatus == 1)
+  {
+    eventData = CLOUD_UPLOAD_INFO;
+    osMessagePut(cloudQueueHandle, eventData, 0);
+  }
+}
+
+void errorTimerFunc(void const *argument)
+{
+
+  if (checkBatErr() == 1)
+  {
+    eventData = CLOUD_UPLOAD_BAT_ERR;
+    osMessagePut(cloudQueueHandle, eventData, 0);
+  }
+
+  if (checkInverterErr() == 1)
+  {
+    eventData = CLOUD_UPLOAD_INVERTER_ERR;
+    osMessagePut(cloudQueueHandle, eventData, 0);
+  }
+}
+
+uint32_t event = 0;
+void cloud_queue_exec(osEvent *osEvent)
+{
+
+  event = (uint32_t)(osEvent->value.v);
 #if 0    
      if( (event & CLOUD_UPLOAD_INFO) !=0 )
      {
@@ -525,36 +507,34 @@ uint16_t delayCnt=0;
      {
        readGnssData();
      }
-     
- 
+
 #endif
- 
+
 #if 1
-     switch(event)
-     {
-        case CLOUD_UPLOAD_INFO:   
-         pubLiveData();
-         break;
-        case CLOUD_UPLOAD_GPS:
-         pubGPSData();
-         break;
-       case CLOUD_UPLOAD_INVERTER_ERR:
-         pubInverterErrData();
-         break;
-       case CLOUD_UPLOAD_BAT_ERR:
-         pubBatErrData();
-         break;
-       case READ_GPS_DATA:
-          readGnssData();
-          break;
-       
-       default:
-       break;
-    }
-    
+  switch (event)
+  {
+  case CLOUD_UPLOAD_INFO:
+    pubLiveData();
+    break;
+  case CLOUD_UPLOAD_GPS:
+    pubGPSData();
+    break;
+  case CLOUD_UPLOAD_INVERTER_ERR:
+    pubInverterErrData();
+    break;
+  case CLOUD_UPLOAD_BAT_ERR:
+    pubBatErrData();
+    break;
+  case READ_GPS_DATA:
+    readGnssData();
+    break;
+
+  default:
+    break;
+  }
+
 #endif
-  
- }
+}
 /* USER CODE END Application */
 
 /************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
